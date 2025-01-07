a9932f7b67eb854afee0f20bd9487d424fd29cca
Cleibel30 <cleibel30052006@gmail.com>
2025-01-07 19:24:45 -0400

    cmabio

--- index.js
+++ index.js
@@ -21,6 +21,14 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); // Cambia '*' por el dominio específico si es necesario
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+  
+
 app.use(express.json())
 app.use(express.static('.'));
 app.use(cors());
